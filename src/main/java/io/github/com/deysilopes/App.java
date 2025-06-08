854c6b05a86af1dfff86907d1dc1da90915e46a9
DeysiLopes <deysilinconl@gmail.com>
2025-06-08 13:08:12 -0300

    projeto array: finaliza com matriz

--- src/main/java/io/github/com/deysilopes/App.java
+++ src/main/java/io/github/com/deysilopes/App.java
@@ -11,15 +11,25 @@ public class App {
         Scanner console = new Scanner(System.in);
         System.out.println("Quantos dias serão inseridos para o calculo de media? ");
         int numDias = console.nextInt();
+        int [] temps = new int[numDias];
+        // armazenar temps e achar média
         int sum = 0;
-        for (int i=1; i<=numDias; i++){
-            System.out.println("Dia " + i + " mãxima temperatura: ");
-            int next = console.nextInt();
-            sum += next;
+        for (int i=0; i<numDias; i++){
+            System.out.println("Dia " + (i+1) + " mãxima temperatura: ");
+            temps[i] = console.nextInt();
+            sum += temps[i];
         }
 
         double media = sum / numDias;
+        // conta dias acima da temperatura
+        int acima = 0;
+        for (int i=0; i< temps.length; i++){
+            if (temps[i]>media){
+                acima++;
+            }
+        }
         System.out.println();
         System.out.println("A temperatura media é: " + media);
+        System.out.println(acima + " Dias acima da temperatura! ");
     }
 }
